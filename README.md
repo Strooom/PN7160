908ac91d2dd09558355cc1e2b0a8d21dfff8a1de
Pascal Roobrouck <pascal.roobrouck@gmail.com>
2024-10-24 10:11:21 +0200

    set up CI workflow

---
+++ README.md
@@ -0,0 +1,4 @@
+# PN7150
+Library/Driver for NXP PN7160 NFC device
+
+All documentation is in the Repository Wiki : https://github.com/Strooom/PN7160/wiki
---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+#include <logging.hpp>
+#include <i2c_t3.h>
+#include <pn7160interface.hpp>
+#include <nci.hpp>
+
+void setup() {
+    logging::initialize();
+    logging::enable(logging::destination::uart1);
+    Wire.begin();
+    PN7160Interface::initialize(2, 3, 0x28);
+    PN7160Interface::reset();
+}
+
+void loop() {
+    NCI::run();
+}
