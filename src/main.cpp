de69d5150ade11662e29c503f11f256417281ac8
Pascal Roobrouck <pascal.roobrouck@gmail.com>
2024-10-29 17:58:10 +0100

    fix linker error

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,15 @@
 void setup() {
     logging::initialize();
     logging::enable(logging::source::stateChanges);
-    //logging::enable(logging::source::nciMessages);
+    // logging::enable(logging::source::nciMessages);
     logging::enable(logging::source::tagEvents);
     logging::enable(logging::source::criticalError);
     logging::enable(logging::destination::uart1);
     Wire.begin();
     PN7160Interface::initialize(2, 3, 0x28);
+    Serial.println("initialized");
 }
 
 void loop() {
     nci::run();
 }
-
