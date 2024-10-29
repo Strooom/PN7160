0b05ca5fbda5074bc3641760096ddfcf7d85e449
Pascal Roobrouck <pascal.roobrouck@gmail.com>
2024-10-29 18:51:30 +0100

    nci unit testing

--- test/generic/test_nci/test.cpp
+++ test/generic/test_nci/test.cpp
@@ -29,9 +29,28 @@ void test_check_status() {
     TEST_ASSERT_TRUE(nci::checkMessageStatus(0x00));
 }
 
+void test_state_machine() {
+    nci::reset();
+    nci::run();
+    TEST_ASSERT_EQUAL(nciState::venResetActive, nci::getState());
+    singleTimer::mockMillis += 5;
+    nci::run();
+    TEST_ASSERT_EQUAL(nciState::waitForResetDone, nci::getState());
+    singleTimer::mockMillis += 5;
+    nci::run();
+    TEST_ASSERT_EQUAL(nciState::waitForCoreResetResponse, nci::getState());
+}
 
 void test_dummy() {
     nci::moveState(nciState::boot);
+    nci::getMessage();
+    nci::checkMessageLength(0);
+    nci::getMessageType(nci::rxBuffer);
+    nci::getGroupIdentifier(nci::rxBuffer);
+    nci::getOpcodeIdentifier(nci::rxBuffer);
+    nci::getMessageId(nci::rxBuffer);
+    nci::timeoutError();
+    nci::unexpectedMessageError();
 }
 
 int main(int argc, char **argv) {
@@ -39,6 +58,7 @@ int main(int argc, char **argv) {
     RUN_TEST(test_initialize);
     RUN_TEST(test_reset);
     RUN_TEST(test_check_status);
+    RUN_TEST(test_state_machine);
     RUN_TEST(test_dummy);
     UNITY_END();
 }
