20d92705c5778941b4e99abc7a9fd8eb5482bd87
Pascal Roobrouck <pascal.roobrouck@gmail.com>
2024-11-02 14:45:17 +0100

    extend unit testing

--- lib/timer/intervaltimer.hpp
+++ lib/timer/intervaltimer.hpp
@@ -3,21 +3,23 @@
 class intervalTimer {
   public:
     void start();
-    void startOrContinue();                // start the intervalTimer if not yet running, do nothing if it already runs
-    void start(unsigned long);             // set a new interval time and start the intervaltimer
-    void set(unsigned long);               // set a new interval time
-    bool expired();                        // check if intervalTimer has expired
-    void stop();                           // disable before expiring
-    bool isRunning() const;                // is the timer running or stopped
-    unsigned long value() const;           // time passed in current interval, in milliseconds
-    unsigned long interval() const;        // current interval, in milliseconds
+    void startOrContinue();                     // start the intervalTimer if not yet running, do nothing if it already runs
+    void start(unsigned long);                  // set a new interval time and start the intervaltimer
+    void set(unsigned long);                    // set a new interval time
+    bool isExpired();                           // check if intervalTimer has expired
+    void stop();                                // disable before expiring
+    bool isRunning() const;                     // is the timer running or stopped
+    unsigned long startTime() const;            // milliseconds
+    unsigned long duration() const;             // current interval, in milliseconds
+    unsigned long timePassed() const;           // time passed in current interval, in milliseconds
+    unsigned long timeRemaining() const;        // time from now to expiration, in milliseconds
 
 #ifndef unitTesting
   private:
 #endif
     bool timerIsRunning{false};
-    unsigned long intervalStartTime{0};
-    unsigned long intervalDuration;
+    unsigned long timerStartTime{0};
+    unsigned long timerDuration{0};
 #ifdef generic
     static unsigned long mockMillis;        // mock millis() function for unit testing
 #endif
--- test/generic/test_singletimer/test.cpp
+++ test/generic/test_singletimer/test.cpp
@@ -7,81 +7,110 @@ void tearDown(void) {
 }
 
 void test_initialize() {
-    singleTimer testTimer;
+    singleShotTimer testTimer;
     TEST_ASSERT_FALSE(testTimer.isRunning());
     TEST_ASSERT_FALSE(testTimer.timerIsRunning);
+    TEST_ASSERT_EQUAL(0, testTimer.startTime());
+    TEST_ASSERT_EQUAL(0, testTimer.duration());
 }
 
-void test_start() {
-    singleTimer::mockMillis = 100;
-    singleTimer testTimer;
-    testTimer.start(200);
+void test_standard_scenario() {        // Start timer, wait until it's expired
+
+    singleShotTimer::mockMillis = 100;
+    singleShotTimer testTimer;
+
+    testTimer.start(50);
     TEST_ASSERT_TRUE(testTimer.isRunning());
-    TEST_ASSERT_TRUE(testTimer.timerIsRunning);
-    TEST_ASSERT_EQUAL(100, testTimer.startTime);
-    TEST_ASSERT_EQUAL(200, testTimer.timerDuration);
-    TEST_ASSERT_EQUAL(200, testTimer.duration());
-    TEST_ASSERT_EQUAL(0, testTimer.value());
-    singleTimer::mockMillis += 100;
-    TEST_ASSERT_EQUAL(100, testTimer.value());
+    TEST_ASSERT_EQUAL(100, testTimer.startTime());
+    TEST_ASSERT_EQUAL(50, testTimer.duration());
+    TEST_ASSERT_EQUAL(0, testTimer.timePassed());
+    TEST_ASSERT_EQUAL(50, testTimer.timeRemaining());
+
+    singleShotTimer::mockMillis += 20;
+    TEST_ASSERT_FALSE(testTimer.isExpired());
+    TEST_ASSERT_TRUE(testTimer.isRunning());
+    TEST_ASSERT_EQUAL(100, testTimer.startTime());
+    TEST_ASSERT_EQUAL(50, testTimer.duration());
+    TEST_ASSERT_EQUAL(20, testTimer.timePassed());
+    TEST_ASSERT_EQUAL(30, testTimer.timeRemaining());
+
+    singleShotTimer::mockMillis += 40;
+    TEST_ASSERT_TRUE(testTimer.isExpired());
+    TEST_ASSERT_FALSE(testTimer.isRunning());
+    TEST_ASSERT_EQUAL(100, testTimer.startTime());
+    TEST_ASSERT_EQUAL(50, testTimer.duration());
+    TEST_ASSERT_EQUAL(0, testTimer.timePassed());
+    TEST_ASSERT_EQUAL(0, testTimer.timeRemaining());
+
+    TEST_ASSERT_FALSE(testTimer.isExpired());        // isExpired only returns true once
+}
+
+void test_early_stop_scenario() {        // Start timer, stop it before it expires
+    singleShotTimer::mockMillis = 100;
+    singleShotTimer testTimer;
+
+    testTimer.start(50);
+    singleShotTimer::mockMillis += 20;
+    testTimer.stop();
+    TEST_ASSERT_FALSE(testTimer.isExpired());
+    TEST_ASSERT_FALSE(testTimer.isRunning());
+    TEST_ASSERT_EQUAL(100, testTimer.startTime());
+    TEST_ASSERT_EQUAL(50, testTimer.duration());
+    TEST_ASSERT_EQUAL(0, testTimer.timePassed());
+    TEST_ASSERT_EQUAL(0, testTimer.timeRemaining());
 }
 
 void test_start_or_continue() {
-    singleTimer::mockMillis = 100;
-    singleTimer testTimer;
-    testTimer.start(200);
-    singleTimer::mockMillis += 100;
-    testTimer.startOrContinue(400);
+    singleShotTimer::mockMillis = 100;
+    singleShotTimer testTimer;
+
+    testTimer.start(50);
     TEST_ASSERT_TRUE(testTimer.isRunning());
-    TEST_ASSERT_EQUAL(100, testTimer.startTime);
-    TEST_ASSERT_EQUAL(200, testTimer.timerDuration);
-    TEST_ASSERT_EQUAL(200, testTimer.duration());
-    testTimer.stop();
-    singleTimer::mockMillis += 100;
-    testTimer.startOrContinue(400);
-    TEST_ASSERT_EQUAL(300, testTimer.startTime);
-    TEST_ASSERT_EQUAL(400, testTimer.timerDuration);
-}
+    TEST_ASSERT_EQUAL(100, testTimer.startTime());
+    TEST_ASSERT_EQUAL(50, testTimer.duration());
+    TEST_ASSERT_EQUAL(0, testTimer.timePassed());
+    TEST_ASSERT_EQUAL(50, testTimer.timeRemaining());
 
+    singleShotTimer::mockMillis += 20;
+    testTimer.startOrContinue(70);
+    TEST_ASSERT_FALSE(testTimer.isExpired());
+    TEST_ASSERT_TRUE(testTimer.isRunning());
+    TEST_ASSERT_EQUAL(100, testTimer.startTime());
+    TEST_ASSERT_EQUAL(50, testTimer.duration());
+    TEST_ASSERT_EQUAL(20, testTimer.timePassed());
+    TEST_ASSERT_EQUAL(30, testTimer.timeRemaining());
 
-void test_stop() {
-    singleTimer::mockMillis = 500;
-    singleTimer testTimer;
-    testTimer.start(200);
-    testTimer.stop();
-    TEST_ASSERT_FALSE(testTimer.isRunning());
-}
-
-void test_expired() {
-    singleTimer::mockMillis = 500;
-    singleTimer testTimer;
-    testTimer.start(200);
-    TEST_ASSERT_FALSE(testTimer.expired());
-    singleTimer::mockMillis = 700;
-    TEST_ASSERT_FALSE(testTimer.expired());
-    singleTimer::mockMillis = 701;
-    TEST_ASSERT_TRUE(testTimer.expired());
+    singleShotTimer::mockMillis += 40;
+    TEST_ASSERT_TRUE(testTimer.isExpired());
+    testTimer.startOrContinue(70);
+    TEST_ASSERT_FALSE(testTimer.isExpired());
+    TEST_ASSERT_TRUE(testTimer.isRunning());
+    TEST_ASSERT_EQUAL(160, testTimer.startTime());
+    TEST_ASSERT_EQUAL(70, testTimer.duration());
 }
 
 void test_expired_and_continue() {
-    singleTimer::mockMillis = 500;
-    singleTimer testTimer;
-    testTimer.start(200);
-    singleTimer::mockMillis = 700;
-    TEST_ASSERT_FALSE(testTimer.expiredAndContinue());
-    singleTimer::mockMillis = 701;
-    TEST_ASSERT_TRUE(testTimer.expiredAndContinue());
-    TEST_ASSERT_TRUE(testTimer.isRunning());
-}
+    singleShotTimer::mockMillis = 100;
+    singleShotTimer testTimer;
 
+    testTimer.start(50);
+    singleShotTimer::mockMillis += 20;
+    TEST_ASSERT_FALSE(testTimer.isExpiredAndContinue());
+    singleShotTimer::mockMillis += 40;
+    TEST_ASSERT_TRUE(testTimer.isExpiredAndContinue());
+    TEST_ASSERT_TRUE(testTimer.isRunning());
+
+    testTimer.stop();
+    TEST_ASSERT_FALSE(testTimer.isExpiredAndContinue());
+}
 
 int main(int argc, char** argv) {
     UNITY_BEGIN();
     RUN_TEST(test_initialize);
-    RUN_TEST(test_start);
+    RUN_TEST(test_standard_scenario);
+    RUN_TEST(test_early_stop_scenario);
     RUN_TEST(test_start_or_continue);
-    RUN_TEST(test_stop);
-    RUN_TEST(test_expired);
     RUN_TEST(test_expired_and_continue);
     UNITY_END();
 }
+
