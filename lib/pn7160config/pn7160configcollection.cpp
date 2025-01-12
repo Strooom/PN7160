b5ee2495a0b8649221b0da3a2cc20bbb8fe81161
Pascal Roobrouck <pascal.roobrouck@gmail.com>
2025-01-12 11:18:37 +0100

    ready for release

--- lib/pn7160config/pn7160configcollection.cpp
+++ lib/pn7160config/pn7160configcollection.cpp
@@ -1,9 +1,9 @@
 #include <pn7160configcollection.hpp>
 
-// payload bytes for PN7160 configs 
+// payload bytes for PN7160 configs
 
-const uint8_t setTotalDurationConfigData[2]{0xF4, 0x01};
-const uint8_t powerManagementConfigData[11]{0x11, 0x01, 0xC1, 0xB1, 0x00, 0xDA, 0x1E, 0x14, 0x00, 0xD0, 0x0C};
+const uint8_t setTotalDurationConfigData[2]{0xF4, 0x01};                                                              // TOTAL_DURATION = 500ms
+const uint8_t powerManagementConfigData[11]{0x11, 0x01, 0xC1, 0xB1, 0x00, 0xDA, 0x1E, 0x14, 0x00, 0xD0, 0x0C};        // PMU_CFG selects CFG1
 
 const pn7160Config setTotalDurationConfig{0x0000, 0x02, setTotalDurationConfigData};
 const pn7160Config powerManagementConfig{0xA00E, 0x0B, powerManagementConfigData};
